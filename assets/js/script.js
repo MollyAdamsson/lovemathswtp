0a8fc39de9f8e976e2bfb06a33615cd7c0693ca5
Molly Adamsson <adamssonmolly@gmail.com>
2023-01-09 10:15:18 +0000

    Add some details and questions development to the numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,9 +13,13 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 });
 
+/**
+ *The main game creates a loop when the page is loaded */
 function runGame () {
-
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 }
+
 
 function checkAnswer() {
 
